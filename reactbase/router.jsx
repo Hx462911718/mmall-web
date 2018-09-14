6225b68d97aae48513392c874c48970a48c347ae
hexin <hx042412@163.com>
2018-09-14 17:18:38 +0800

    “react框架基础

--- reactbase/router.jsx
+++ reactbase/router.jsx
@@ -1,3 +1,34 @@
+/*
+* @Author: hexin
+* @Date:   2018-01-19 23:22:32
+* @Last Modified by:   hexin
+* @Last Modified time: 2018-01-20 00:11:34
+*/
+
+// 页面路由
+window.location.href = 'http://www.baidu.com';
+history.back();
+
+// hash 路由
+window.location = '#hash';
+window.onhashchange = function(){
+    console.log('current hash:', window.location.hash);
+}
+
+// h5 路由
+// 推进一个状态
+history.pushState('name', 'title', '/path');
+// 替换一个状态
+history.replaceState('name', 'title', '/path');
+// popstate
+window.onpopstate = function(){
+    console.log(window.location.href);
+    console.log(window.location.pathname);
+    console.log(window.location.hash);
+    console.log(window.location.search);
+}
+
+
 // react-router
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -14,16 +45,16 @@ class A extends React.Component{
             Component A
             
             <Switch>
+                {/* exact 完全匹配*/}
                 <Route exact path={`${this.props.match.path}`} render={(route) => {
                     return <div>当前组件是不带参数的A</div>
                 }}/>
                 <Route path={`${this.props.match.path}/sub`} render={(route) => {
                     return <div>当前组件是Sub</div>
                 }}/>
-               <Route path={`${this.props.match.path}/:id`} render={(route)=>{
-					return <div>当前组件是带参数的A,参数：{route.match.params.id}</div>
-					
-               }}/>
+                <Route path={`${this.props.match.path}/:id`} render={(route) => {
+                    return <div>当前组件是带参数的A, 参数是：{route.match.params.id}</div>
+                }}/>
             </Switch>
             </div>
         )
